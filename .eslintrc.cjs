ad3a07e18236c92948c85fb752b273d3a2362c71
jsun969 <i@jsun.lol>
2024-02-01 22:56:43 +1030

    :technologist: format eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,36 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["./tsconfig.json"],
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:astro/recommended",
-  ],
-  overrides: [
-    {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {},
-    },
-  ],
-  ignorePatterns: ["*.cjs"],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { ignoreRestSiblings: true }],
-    "@typescript-eslint/consistent-type-imports": "warn",
-    "no-console": "warn",
-  },
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['./tsconfig.json'],
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'plugin:astro/recommended',
+	],
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			rules: {},
+		},
+	],
+	ignorePatterns: ['*.cjs', '*.mjs'],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{ disallowTypeAnnotations: false },
+		],
+		'no-console': 'warn',
+	},
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,21 +1,21 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions & import('@trivago/prettier-plugin-sort-imports').PluginConfig} */
 module.exports = {
-  singleQuote: true,
-  trailingComma: "all",
-  plugins: [
-    "@trivago/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-    "prettier-plugin-astro",
-  ],
-  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
-  importOrderSeparation: true,
-  useTabs: true,
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	plugins: [
+		'@trivago/prettier-plugin-sort-imports',
+		'prettier-plugin-tailwindcss',
+		'prettier-plugin-astro',
+	],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+	importOrderSeparation: true,
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
 };
