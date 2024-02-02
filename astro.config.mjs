172a6c37cb1df28875d76f511914624da7409fe8
jsun969 <i@jsun.lol>
2024-02-03 00:12:38 +1030

    :wrench: add htmx integration and set output to `server`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import alpinejs from '@astrojs/alpinejs';
 import tailwind from '@astrojs/tailwind';
+import htmx from 'astro-htmx';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), alpinejs()],
+	integrations: [tailwind(), alpinejs(), htmx()],
+	output: 'server',
 });
